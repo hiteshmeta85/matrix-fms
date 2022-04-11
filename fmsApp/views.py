33b1b63275a6379014585d1fd14d6223f837574b
Hitesh Meta <meta.hitesh85@gmail.com>
2022-04-11 22:32:37 +0530

    remove update profile and password

--- fmsApp/views.py
+++ fmsApp/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import json
 from django.contrib import messages
-from django.contrib.auth.models import User
 from django.http import HttpResponse
-from fmsApp.forms import UserRegistration, SavePost, UpdateProfile, UpdatePasswords
+from fmsApp.forms import UserRegistration, SavePost
 from fmsApp.models import Post
 from cryptography.fernet import Fernet
 from django.conf import settings
@@ -145,7 +144,6 @@ def delete_post(request):
 
 
 def shareF(request, id=None):
-    # print(str("b'UdhnfelTxqj3q6BbPe7H86sfQnboSBzb0irm2atoFUw='").encode())
     context['page_title'] = 'Shared File'
     if not id is None:
         key = settings.ID_ENCRYPTION_KEY
@@ -157,41 +155,3 @@ def shareF(request, id=None):
         context['page_title'] += str(" - " + post.title)
 
     return render(request, 'share-file.html', context)
-
-
-@login_required
-def update_profile(request):
-    context['page_title'] = 'Update Profile'
-    user = User.objects.get(id=request.user.id)
-    if not request.method == 'POST':
-        form = UpdateProfile(instance=user)
-        context['form'] = form
-        print(form)
-    else:
-        form = UpdateProfile(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Profile has been updated")
-            return redirect("profile")
-        else:
-            context['form'] = form
-
-    return render(request, 'manage_profile.html', context)
-
-
-@login_required
-def update_password(request):
-    context['page_title'] = "Update Password"
-    if request.method == 'POST':
-        form = UpdatePasswords(user=request.user, data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Your Account Password has been updated successfully")
-            update_session_auth_hash(request, form.user)
-            return redirect("profile")
-        else:
-            context['form'] = form
-    else:
-        form = UpdatePasswords(request.POST)
-        context['form'] = form
-    return render(request, 'update_password.html', context)
